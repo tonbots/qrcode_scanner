c8aab407ea62438880746e38ff4eb68164050fde
tonbots <kartono.saleh@gmail.com>
2021-12-01 10:21:39 +0700

    QR Code

--- App.js
+++ App.js
@@ -1,56 +1,13 @@
-import React from 'react';
-import {Component} from 'react';
-import {Linking, StyleSheet, Text, TouchableOpacity} from 'react-native';
-import {RNCamera} from 'react-native-camera';
-import QRCodeScanner from 'react-native-qrcode-scanner';
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {QrCode} from './src';
 
 class App extends Component {
-  onSuccess = e => {
-    Linking.openURL(e.data).catch(err =>
-      console.error('An error occured', err),
-    );
-  };
-
   render() {
-    return (
-      <QRCodeScanner
-        onRead={this.onSuccess}
-        flashMode={RNCamera.Constants.FlashMode.torch}
-        topContent={
-          <Text style={styles.centerText}>
-            Go to{' '}
-            <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on
-            your computer and scan the QR code.
-          </Text>
-        }
-        bottomContent={
-          <TouchableOpacity style={styles.buttonTouchable}>
-            <Text style={styles.buttonText}>OK. Got it!</Text>
-          </TouchableOpacity>
-        }
-      />
-    );
+    return <QrCode />;
   }
 }
 
 export default App;
 
-const styles = StyleSheet.create({
-  centerText: {
-    flex: 1,
-    fontSize: 18,
-    padding: 32,
-    color: '#777',
-  },
-  textBold: {
-    fontWeight: '500',
-    color: '#000',
-  },
-  buttonText: {
-    fontSize: 21,
-    color: 'rgb(0,122,255)',
-  },
-  buttonTouchable: {
-    padding: 16,
-  },
-});
+const styles = StyleSheet.create({});
